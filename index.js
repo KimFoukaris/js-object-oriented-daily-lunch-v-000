9ea27fda8a8d682ef932c7627973c659b771ca21
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-01 20:18:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ class Meal {
     })
   }
   static byPrice() {
-    return this.sort(function (a, b) {
+    return store.meals.sort(function (a, b) {
       return a.price - b.price;
     });
   }
